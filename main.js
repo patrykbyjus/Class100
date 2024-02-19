b6ee3dd7048453ddb747a8e26a157c5d1bb30df7
patrykbyjus <139406101+patrykbyjus@users.noreply.github.com>
2024-02-19 17:26:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var SpeechRecognition=window.webkitSpeechRecognition
+var recognition=new SpeechRecognition()
+function start(){
+    document.getElementById("textbox").innerHTML=""
+    recognition.start()
+}
+recognition.onresult=function(event){
+    var content=event.results[0][0].transcript
+    document.getElementById("textbox").innerHTML=content
+}
+if(content=="take my selfie"){
+        speak()
+    }
+    
+}
+function speak(){
+    var synth=window.speechSynthesis
+    var speakdata="taking your selfie in five seconds."
+    var utterthis=new SpeechSynthesisUtterance(speakdata)
+    synth.speak(utterthis)
+    Webcam.attach(camera)
+    setTimeout(function(){
+take_snapshot()
+save()
+    },5000)
+}
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+})
+camera=document.getElementById("camera")
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'">'
+    })
+}
+function save(){
+    link=document.getElementById("link")
+    image=document.getElementById("selfie_image").src
+    link.href=image
+    link.click()
+}
